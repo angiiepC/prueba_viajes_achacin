b5718c248298f91acca449bdc9438d874ace3a33
angie chacin <achacinp@gmail.com>
2022-11-20 21:21:16 -0300

    Se  agrego JS

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var navbarTop = $(window).scrollTop()
+        if(navbarTop > 700){
+            $(".navbar").addClass("navbar-white");
+        } else {
+            $(".navbar").removeClass("navbar-white")
+        }
+    });
+});
+
+$(".submit").click(function () {
+    alert("Información enviada con exito")
+});
+
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
